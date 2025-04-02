0f9cf3a60b596a67545459570c02607490d64169
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2025-04-02 11:30:34 +0200

    first

--- headers/roots.h
+++ headers/roots.h
@@ -6,7 +6,9 @@ typedef struct{
 } error_id;
 
 typedef struct{
-
+    char *file_name;
+    long int bite_size;
+    int ls_size;
 } file_info;
 
 int parse_commands(int n, char *v[], FILE **input, FILE **output);
